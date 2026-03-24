8e95eb4eab48b73d47c45ab603c66d708bcdf8f7
Swapnil <swapnil.chettipally@gmail.com>
2026-03-25 00:46:12 +0530

    fix db connection pool

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,14 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
-const db = mysql.createConnection(process.env.MYSQL_URL);
-
-db.connect((err) => {
-  if (err) {
-    console.error("❌ MySQL connection failed:", err);
-  } else {
-    console.log("✅ Connected to Railway MySQL");
-  }
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-module.exports = db;
+module.exports = pool;
